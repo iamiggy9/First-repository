daeef2e5cc1fa3286bb374b30af5e941e18d720a
iamiggy9 <iarrranz9@gmail.com>
2021-03-02 21:50:06 +0100

    Moved files to correct folders

--- P0/Ex4.py
+++ P0/Ex4.py
@@ -1,11 +1,11 @@
 from Seq0 import *
 
 bases = ["A", "C", "T", "G"]
-list_of_genes = ["U5", "ADA", "FRAT1", "FXN", "RNU6_269P" ]
+list_of_genes = ["U5", "ADA", "FRAT1", "FXN", "RNU6_269P"]
 txt = ".txt"
 FOLDER = "../SESSION-04/"
 
 for e in list_of_genes:
     print("Gene", e, ":")
     for i in bases:
-        print( i, ":", seq_count_base(seq_read_fasta(FOLDER+e+txt), i))
+        print(i, ":", seq_count_base(seq_read_fasta(FOLDER+e+txt), i))
